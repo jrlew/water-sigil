6b459921620cecee042431ca5735111b721583f5
krybl <krybl22@gmail.com>
2018-11-20 19:43:51 -0500

    Added cobblestone texture and new unit types

--- data/terrain/plain.py
+++ data/terrain/plain.py
@@ -6,5 +6,5 @@ from .terrain import Terrain
 
 class Plain(Terrain):
     def __init__(self):
-        self = Terrain.__init__(self, "Plain", "../../assets/terrain/grass-background.png", 0, 0)
+        self = Terrain.__init__(self, "Plain", "../../assets/terrain/cobblestone.png", 0, 0)
 
